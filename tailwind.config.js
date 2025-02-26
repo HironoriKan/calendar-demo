19fd2cf0fc718fd61d0ef784b57b7964ab59b174
Hironori Kan <kanbook@GUjpPX311491932.localdomain>
2025-02-26 18:02:37 +0900

    UI改善: MakeMeロゴ追加、フォント変更、スクロール動作最適化、レイアウト調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans JP', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } 
