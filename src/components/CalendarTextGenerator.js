549ee24314a325a55551ab4a0d49e3134cd5fb2e
Hironori Kan <kanbook@GUjpPX311491932.localdomain>
2025-02-26 17:45:59 +0900

    “不要アイコンの削除”

--- src/components/CalendarTextGenerator.js
+++ src/components/CalendarTextGenerator.js
@@ -631,7 +631,7 @@ const CalendarTextGenerator = () => {
         onMouseUp={handleMouseUp}
       >
         {/* ヘッダー - 固定高さ */}
-        <div className="bg-white p-2 flex justify-between items-center shadow-sm">
+        <div className="bg-white p-2 flex justify-center items-center shadow-sm">
           <div className="flex items-center">
             <div className="text-lg font-medium text-gray-500 flex items-center">
               <svg width="40" height="24" viewBox="0 0 40 24">
@@ -644,23 +644,6 @@ const CalendarTextGenerator = () => {
               </svg>
               <span className="ml-2 font-bold">カレンダー日程調整</span>
             </div>
-          </div>
-          <div className="flex items-center space-x-1">
-            <button className="p-1 text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-              </svg>
-            </button>
-            <button className="p-1 text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-              </svg>
-            </button>
-            <button className="p-1 text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
           </div>
         </div>
         
@@ -817,7 +800,7 @@ const CalendarTextGenerator = () => {
           </div>
           
           {/* フッターボタン */}
-          <div className="bg-white p-1 flex justify-end items-center border-t border-gray-200">
+          <div className="bg-white p-1 flex justify-center items-center border-t border-gray-200">
             <div className="flex space-x-3">
               <button 
                 onClick={resetSelection}
